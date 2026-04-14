ab09a768d967016e5392c82dbf41b519aa37f3d2
soheekim <kim.sohee102@gmail.com>
2026-04-13 23:57:34 -0600

    add slideshow for all image categories;

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,9 +32,29 @@
 /* image slideshows */
 
 let truffles = document.getElementById('Truffles-image');
-let imagenum = 0;
+let truffles_imagenum = 0;
+
+let run = document.getElementById('Run-image');
+let run_imagenum = 0;
+
+let youth = document.getElementById('Youth-image');
+let youth_imagenum = 0;
+
+let camp = document.getElementById('Camp-image');
+let camp_imagenum = 0;
 
 setInterval(() => {;
-	truffles.src = `./abtme_pg_media/Truffles/Truffles${imagenum+1}.jpeg`;
-	imagenum = (imagenum +1)%6;
+	truffles.src = `./abtme_pg_media/Truffles/Truffles${truffles_imagenum+1}.jpeg`;
+	truffles_imagenum = (truffles_imagenum +1)%6;
+
+	run.src = `./abtme_pg_media/Run/Run${run_imagenum+1}.jpeg`;
+	run_imagenum = (run_imagenum +1)%8;
+
+	youth.src = `./abtme_pg_media/Youth/Youth${youth_imagenum+1}.jpg`;
+	youth_imagenum = (youth_imagenum +1)%4;
+
+	camp.src = `./abtme_pg_media/Camp/Camp${camp_imagenum+1}.JPG`;
+	camp_imagenum = (camp_imagenum +1)%7;
 }, 3000);
+
+
